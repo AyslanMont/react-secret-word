ab09ebad287e4422996fe00236d0a3eddd3768c9
Ayslan Francisco Monteiro Neto <ayslanfrancisco999@gmail.com>
2025-07-05 19:11:07 -0300

    style: Finalizando tela inicial

---
+++ src/data/words.js
@@ -0,0 +1,9 @@
+export const wordList = {
+  frutas: ['banana', 'maçã', 'uva', 'melancia', 'abacaxi', 'laranja'],
+  animais: ['cachorro', 'gato', 'elefante', 'girafa', 'tigre', 'leão'],
+  cores: ['vermelho', 'azul', 'amarelo', 'verde', 'preto', 'branco'],
+  paises: ['brasil', 'canadá', 'japão', 'alemanha', 'austrália', 'méxico'],
+  objetos: ['cadeira', 'garfo', 'janela', 'telefone', 'bola', 'computador'],
+  profissoes: ['médico', 'professor', 'engenheiro', 'artista', 'bombeiro'],
+  esportes: ['futebol', 'basquete', 'tênis', 'natação', 'vôlei', 'surf']
+};
