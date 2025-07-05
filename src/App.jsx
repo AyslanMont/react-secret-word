c7c6653aca36af7e32ada3a515844f0ffb75ad33
Ayslan Francisco Monteiro Neto <ayslanfrancisco999@gmail.com>
2025-07-05 20:15:59 -0300

    fix: Criando componentes do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import './App.css'
-import StartScreen from './components/StartScreen'
+//  CSS
+import "./App.css";
+
+// React
+import { useCallback, useEffect, useState } from "react";
+
+// Data
+import { wordList } from "./data/words";
+
+// Components
+import StartScreen from "./components/StartScreen";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordList);
 
   return (
-    <div className='App'>
-      <StartScreen></StartScreen>
+    <div className="App">
+      {gameStage === "start" && <StartScreen></StartScreen>}
     </div>
-  )
+  );
 }
-export default App
+export default App;
--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,11 +1,12 @@
-import "./StartScreen.css"
+// CSS
+import "./StartScreen.css";
 
-export default function StartScreen(){
-    return (
-        <div className="start">
-            <h1>Secret Word</h1>
-            <p>Clique no botão abaixo para começar a jogar!</p>
-            <button>Começar o jogo</button>
-        </div>
-    );
+export default function StartScreen() {
+  return (
+    <div className="start">
+      <h1>Secret Word</h1>
+      <p>Clique no botão abaixo para começar a jogar!</p>
+      <button>Começar o jogo</button>
+    </div>
+  );
 }
