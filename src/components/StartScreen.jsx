d900efa2efc3bb9d5038eb7a4a2c26ece92cb466
Ayslan Francisco Monteiro Neto <ayslanfrancisco999@gmail.com>
2025-07-06 13:03:22 -0300

    style: Adicionando elementos de selecionar dificuldade

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -5,6 +5,14 @@ export default function StartScreen({ startGame }) {
   return (
     <div className="start">
       <h1>Secret Word</h1>
+      <p>Selecione a difículdade: </p>
+      <select name="difficulty" className="options">
+        <option value="easy" selected>
+          Fácil
+        </option>
+        <option value="middle">Médio</option>
+        <option value="hard">Difícil</option>
+      </select>
       <p>Clique no botão abaixo para começar a jogar!</p>
       <button onClick={startGame}>Começar o jogo</button>
     </div>
